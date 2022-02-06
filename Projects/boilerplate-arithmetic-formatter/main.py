c7eebcc3b46b68b3d5c08ad25fb802ae9ff42f7f
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-02-06 15:10:05 +0800

    Updated

--- Projects/boilerplate-arithmetic-formatter/main.py
+++ Projects/boilerplate-arithmetic-formatter/main.py
@@ -1,10 +1,10 @@
 # This entrypoint file to be used in development. Start by reading README.md
-from unittest import main #pytest
+from pytest import main #pytest
 
 from arithmetic_arranger import arithmetic_arranger
 
 
-print(arithmetic_arranger(['1 + 2', '1 - 9380']))
+print(arithmetic_arranger(['1 + 2', '1 - 9380'], True))
 
 
 # Run unit tests automatically
